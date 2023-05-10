db1642edcd2d352f6833a954d7dcf6f07fa9a864
VladosNasos <bezzzubkovlad@gmail.com>
2023-05-10 23:06:01 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] array = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","1","2","3","4","5","6","7","8","9","0","!","&","$","?",".","," };
+            Console.Title = "The Matrix";
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            while (true)
+            {
+                
+                Thread.Sleep(15);
+                for (int i = 0; i < 3000; i++)
+                {
+                    int a;
+                    var rand = new Random();
+
+                    var bytes = new byte[48];
+                    rand.NextBytes(bytes);
+
+
+                    a = rand.Next(36);
+                    Console.Write(" "); Console.Write(array[a]); Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    } }
